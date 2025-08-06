f9058af6d975f2c813a5fe999e45fd5ab15a8e57
Rodrigo Rossiter Guimarães Filho <rfilho@informa.com.br>
2025-08-06 14:22:09 -0300

    config: disable Next.js dev indicators

--- gradlock-front/next.config.ts
+++ gradlock-front/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Configuração para desenvolvimento
   reactStrictMode: true,
+  devIndicators: false,
 };
 
 export default nextConfig;
