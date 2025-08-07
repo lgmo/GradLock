b27c33b1b2f2a1d01472080e15e23a3d0a4086bc
Júlio César de Araújo Sobral Filho <jcasf@cin.ufpe.br>
2025-08-07 11:56:06 -0300

    fix: routes

--- gradlock-back/src/routes/api.ts
+++ gradlock-back/src/routes/api.ts
@@ -1,41 +1,16 @@
 import express from 'express';
-import { RoomsController } from '../controllers/roomsController';
-import { ReservationsController } from '../controllers/reservationsController';
-import { authenticate, authorization } from '../middlewares/auth-middlewares';
-import { UserType } from '../../generated/prisma';
+import authRoutes from './modules/auth-routes';
+import userRoutes from './modules/user-routes';
+import roomRoutes from './modules/room-routes';
+import reservationRouter from './modules/reservation-routes';
 
 const apiRouter = express.Router();
 
-apiRouter.get(
-  '/rooms',
-  authenticate,
-  authorization([UserType.STUDENT, UserType.TEACHER]),
-  RoomsController.getAllRooms,
-);
-apiRouter.get(
-  '/rooms/:id',
-  authenticate,
-  authorization([UserType.STUDENT, UserType.TEACHER]),
-  RoomsController.getRoomById,
-);
+// Montagem das rotas organizadas por domínio
+apiRouter.use('/auth', authRoutes);
+apiRouter.use('/users', userRoutes);
+apiRouter.use('/rooms', roomRoutes);
+apiRouter.use('/reservations', reservationRouter);
 
-apiRouter.get(
-  '/reservations/search',
-  authenticate,
-  authorization([UserType.STUDENT, UserType.TEACHER, UserType.ADMIN]),
-  ReservationsController.getReservationsByFilter,
-);
-apiRouter.get(
-  '/reservations/',
-  authenticate,
-  authorization([UserType.ADMIN]),
-  ReservationsController.getAllReservations,
-);
-
-apiRouter.post('/rooms', authenticate, authorization(), RoomsController.createRoom);
-apiRouter.put('/rooms/:id', authenticate, authorization(), RoomsController.updateRoom);
-apiRouter.delete('/rooms/:id', authenticate, authorization(), RoomsController.deleteRoom);
-
-apiRouter.post('/reservations', authenticate, authorization(), ReservationsController.createReservation);
-apiRouter.delete('/reservations/:id', authenticate, authorization(), ReservationsController.deleteReservation);
 export default apiRouter;
+  
