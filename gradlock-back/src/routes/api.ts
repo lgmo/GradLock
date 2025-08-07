11cea155adde8ad73544cfed8279236661ca7d0f
Júlio César de Araújo Sobral Filho <jcasf@cin.ufpe.br>
2025-08-07 12:42:12 -0300

    merge 1

--- gradlock-back/src/routes/api.ts
+++ gradlock-back/src/routes/api.ts
@@ -1,13 +1,41 @@
 import express from 'express';
-import authRoutes from './modules/auth-routes';
-import userRoutes from './modules/user-routes';
-import roomRoutes from './modules/room-routes';
+import { RoomsController } from '../controllers/roomsController';
+import { ReservationsController } from '../controllers/reservationsController';
+import { authenticate, authorization } from '../middlewares/auth-middlewares';
+import { UserType } from '../../generated/prisma';
 
 const apiRouter = express.Router();
 
-// Montagem das rotas organizadas por domínio
-apiRouter.use('/auth', authRoutes);
-apiRouter.use('/users', userRoutes);
-apiRouter.use('/rooms', roomRoutes);
+apiRouter.get(
+  '/rooms',
+  authenticate,
+  authorization([UserType.STUDENT, UserType.TEACHER]),
+  RoomsController.getAllRooms,
+);
+apiRouter.get(
+  '/rooms/:id',
+  authenticate,
+  authorization([UserType.STUDENT, UserType.TEACHER]),
+  RoomsController.getRoomById,
+);
 
+apiRouter.get(
+  '/reservations/search',
+  authenticate,
+  authorization([UserType.STUDENT, UserType.TEACHER, UserType.ADMIN]),
+  ReservationsController.getReservationsByFilter,
+);
+apiRouter.get(
+  '/reservations/',
+  authenticate,
+  authorization([UserType.ADMIN]),
+  ReservationsController.getAllReservations,
+);
+
+apiRouter.post('/rooms', authenticate, authorization(), RoomsController.createRoom);
+apiRouter.put('/rooms/:id', authenticate, authorization(), RoomsController.updateRoom);
+apiRouter.delete('/rooms/:id', authenticate, authorization(), RoomsController.deleteRoom);
+
+apiRouter.post('/reservations', authenticate, authorization(), ReservationsController.createReservation);
+apiRouter.delete('/reservations/:id', authenticate, authorization(), ReservationsController.deleteReservation);
 export default apiRouter;
